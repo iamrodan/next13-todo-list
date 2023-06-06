02dd28e3fbe0f8ff030b5872e80e88018d45e137
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-06-06 13:24:22 +0545

    fix(LinkButton): fix to use to props for href rather than hardcoded value

--- src/app/components/LinkButton.tsx
+++ src/app/components/LinkButton.tsx
@@ -7,12 +7,12 @@ type LinkButtonProps = {
   [key: string]: any;
 };
 
-export default function LinkButton({ children, ...rest }: LinkButtonProps) {
+export default function LinkButton({ children, to, ...rest }: LinkButtonProps) {
   return (
     <>
       <Link
         className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-        href="/new"
+        href={to}
         {...rest}
       >
         {children}
