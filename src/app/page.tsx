b79c4b79e816671b43323be40bd9e211d46bd10f
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-06-06 13:16:39 +0545

    feat: create TodoItem component and use it to display todo items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import Link from "next/link";
 import LinkButton from "./components/LinkButton";
+import { prisma } from "@/db";
+import TodoItem from "./components/TodoItem";
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+export default async function Home() {
+  const todos = await getTodos();
+
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2x1">Todos</h1>
         <LinkButton to="/new">New</LinkButton>
       </header>
-      <ul></ul>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
     </>
   );
 }
